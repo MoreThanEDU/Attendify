5123fc4ffaf33b91e8de71cb7d85a168069a8d44
WhyTellMeWhy <161310831+hanwoo0104@users.noreply.github.com>
2025-04-04 16:38:16 +0900

    내부 80번포트

--- index.js
+++ index.js
@@ -1907,6 +1907,6 @@ app.get("/nostatus/:lec_code/:session/", (req, res) => {
         return res.send("<script>history.back();</script>");
 });
 
-https.createServer(options, app).listen(3000, () => {
+https.createServer(options, app).listen(80, () => {
     console.log("실행 중!");
 });
